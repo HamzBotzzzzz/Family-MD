ed2d2d498dbfe7dc33a66b016d550f34ed39e26a
Hamzzzzz <109900268+HamzBotzzzzz@users.noreply.github.com>
2022-09-01 14:29:19 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,11 +2,11 @@ const qrku = "https://telegra.ph/file/f4ee3c55fd67cbd3ea861.jpg"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
 ╭─「 Donasi • Dana 」
-│ • Telkomsel [62895327934887]
-│ • Dana  [62895327934887]
+│ • Smartfren [6288214989352]
+│ • Dana  [6288214989352]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895327934887
+│ > Ingin donasi? Wa.me/6288214989352
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
